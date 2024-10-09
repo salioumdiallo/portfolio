c36cdc4cd7884faaf2eba35762929d259f631abd
Mamadou saliou diallo <salioumdiallo@gmail.com>
2024-10-09 14:39:34 +0200

    three commit

--- script.js
+++ script.js
@@ -1,38 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const mainNav = document.querySelector('.main-nav');
+
+    burgerMenu.addEventListener('click', function() {
+        mainNav.classList.toggle('active');
+    });
+
+    // Formulaire - Feedback après soumission
     const form = document.querySelector('form');
     form.addEventListener('submit', function(event) {
-        event.preventDefault();
         alert('Merci de m\'avoir contacté, je reviendrai vers vous bientôt.');
-        form.reset();
     });
-});
-document.addEventListener('DOMContentLoaded', function () {
-    let slides = document.querySelectorAll('.testimonial-slide');
-    let dots = document.querySelectorAll('.dot');
-    let currentIndex = 0;
-
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
-            slide.classList.toggle('active', i === index);
-            dots[i].classList.toggle('active', i === index);
-        });
-    }
-
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % slides.length;
-        showSlide(currentIndex);
-    }
-
-    dots.forEach((dot, i) => {
-        dot.addEventListener('click', () => {
-            currentIndex = i;
-            showSlide(currentIndex);
-        });
-    });
-
-    showSlide(currentIndex);
-    setInterval(nextSlide, 5000); // Change de témoignage toutes les 5 secondes
-});
-document.querySelector('.burger-menu').addEventListener('click', function() {
-    document.querySelector('.main-nav').classList.toggle('active');
 });
